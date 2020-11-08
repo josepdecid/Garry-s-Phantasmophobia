dd5a7251a935faf2994c7a8fe6da141c801c4910
agrizzli <arthur_mueller@gmx.net>
2020-11-08 23:19:59 +0100

    Asset: Door Key

---
+++ Assets/RotateKeyScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateKeyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.left, 1.0f);
+    }
+}
