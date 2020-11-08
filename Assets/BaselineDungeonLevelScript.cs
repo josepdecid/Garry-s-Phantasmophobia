2427aa52813caf46d5d10f8b9175978610d67e7e
agrizzli <arthur_mueller@gmx.net>
2020-11-08 23:22:34 +0100

    Scene: Baseline dungeon (empty)

---
+++ Assets/BaselineDungeonLevelScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaselineDungeonLevelScript : MonoBehaviour
+{
+    public int sizeX;
+    public int sizeZ;
+
+    // Storage for floor tiles
+    public Dictionary<string,GameObject> planes = new Dictionary<string,GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Remove all generated objects
+    public void CleanUpFloor() {
+        // Cleanup according to saved planes on runtime
+        foreach (var key in planes.Keys)
+        {
+            GameObject plane = planes[key];
+            GameObject.DestroyImmediate(plane);
+        }
+
+        planes.Clear();
+
+        // Cleanup all possibly existing other planes
+        foreach(Transform child in GetComponentsInChildren<Transform>()) {
+            if(child.gameObject.tag == "Floor") {
+                // Debug.Log(child.gameObject.name);
+                GameObject.DestroyImmediate(child.gameObject);
+            }
+        }
+    }
+
+    // Cleanup all objects
+    public void CleanUp() {
+        // Cleanup floor
+        // CleanUpFloor();
+    
+        // Cleanup all possibly existing other planes
+        foreach(Transform child in GetComponentsInChildren<Transform>()) {
+            if(child != transform){
+                // Debug.Log(child.gameObject.name);
+                GameObject.DestroyImmediate(child.gameObject);
+            }
+        }
+    }
+
+    // Build dungeon's matrix floor
+    public void BuildDungeonFloor() {
+        
+        // First cleanup 
+        CleanUpFloor();
+
+        /// Spawn multiple planes in x-z plane
+        for(int x = 0; x < sizeX; x++) {
+            for(int z = 0; z < sizeZ; z++) {
+                // Create plane with tag
+                GameObject plane  = GameObject.CreatePrimitive(PrimitiveType.Plane);
+                string label = "Plane_" + x.ToString() + "_" + z.ToString();
+                plane.name = label;
+                plane.tag = "Floor";
+
+                // Set parent, scale and position
+                plane.transform.SetParent(transform);
+                plane.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                plane.transform.position = new Vector3(2.5f + x * 5, 0, 2.5f + z * 5);
+
+                // Add to list for tracking
+                planes.Add(label, plane);
+            }
+        }
+
+    }
+}
---
+++ Assets/Editor/BaselineDungeonLevelScriptEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BaselineDungeonLevelScript))]
+public class BaselineDungeonLevelScriptEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        // Get target level script from dungeon object
+        BaselineDungeonLevelScript levelScript = (BaselineDungeonLevelScript) target;
+
+        // Add control fields by overriding propeties
+        levelScript.sizeX = EditorGUILayout.IntField("Size x", levelScript.sizeX);
+        levelScript.sizeZ = EditorGUILayout.IntField("Size z", levelScript.sizeZ);
+
+        if(GUILayout.Button("Build dungeon floor")){
+            levelScript.BuildDungeonFloor();
+        }
+
+        if(GUILayout.Button("Clean up floor")){
+            levelScript.CleanUpFloor();
+        }
+
+        if(GUILayout.Button("Clean up all")){
+            levelScript.CleanUp();
+        }
+    }
+}
