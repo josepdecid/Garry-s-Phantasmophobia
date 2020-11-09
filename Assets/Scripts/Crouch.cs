5ba09e9db2349b85eac9913553753ba2af94cae5
David Curto <dcurto95@gmail.com>
2020-11-09 19:15:02 +0100

    First commit + Crouch implemented

---
+++ Assets/Scripts/Crouch.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crouch : MonoBehaviour
+{
+    public KeyCode crouchKey = KeyCode.C;
+    private CharacterController characterController;
+    private bool isCrouched = false;
+    private float originalHeight;
+    private float crouchHeight = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        originalHeight = characterController.height;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(crouchKey))
+        {
+            isCrouched = !isCrouched;
+            UpdateHeight();
+        }
+    }
+
+    void UpdateHeight()
+    {
+        if (isCrouched)
+        {
+            characterController.height = crouchHeight;
+        }
+        else
+        {
+            characterController.height = originalHeight;
+        }
+    }
+}
