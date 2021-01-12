f930d5d2278799c005bad71edd07c6e40c1af1b3
agrizzli <arthur_mueller@gmx.net>
2021-01-12 21:42:35 +0100

    Replace lightnings by thunders only

---
+++ Assets/Atmosphere/Scripts/RandomThunders.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomThunders : MonoBehaviour
+{
+    public float flashProbPerFrame = 0.005f;
+    private int baseRandomRange = 2000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioSource source = GetComponent<AudioSource>();
+        if(source.isPlaying) {
+            return;
+        }
+
+        int flashProb = Random.Range(0, baseRandomRange);
+        if (flashProb < flashProbPerFrame * baseRandomRange) {
+            // Play sound if not already playing
+            source.Play(0);
+        }
+    }
+}
